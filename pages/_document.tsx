a575a7498b7131267386793bff162d477c904258
hawnbin <dngh0825@naver.com>
2022-11-04 15:51:37 +0900

    Add pwa icons , slashcreens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,33 @@
-import Document, {Html ,Head , Main, NextScript} from "next/document";
+import {Html ,Head , Main, NextScript} from "next/document";
 
 export default function MyDocument() {
-    console.log("DOCUMENT IS RUNNING");
     return (
       <Html lang="ko">
         <Head>
+        <meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta name="keywords" content="포켓몬 도감" />
+					<meta name="author" content="유환빈"></meta>
+					<meta name="apple-mobile-web-app-capable" content="yes"></meta>
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content="default"
+					></meta>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="images/icons/icon-192x192.png"></link>
+        <meta name="msapplication-TileColor" content="#fefefe"></meta>  
+        <meta name="theme-color" content="#db5945" />
+
+        <link href="images/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="images/splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         </Head>
         <body>
           <Main />
